c330837d538a455e6168d8d12d371d0a09cb0c3d
Keanunovaa <152877297+Keanunovaa@users.noreply.github.com>
2023-12-05 00:33:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
- question.innerHTML = "Aaaaa, ku juga sukak sama bebooo ";
+ question.innerHTML = "Aaaa, ku juga sukak sama bebooo";
  gif.src =
 "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
