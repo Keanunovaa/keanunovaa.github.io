f5f7c79f6e1e79551a141fd19c357832e07c9959
Keanunovaa <152877297+Keanunovaa@users.noreply.github.com>
2023-12-04 23:25:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
-const gif document.querySelector(".gif");
+const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
-yesBtn.addEventListener("click", () {
-question.innerHTML = "Aaaaa, I like you too";
-gif.src =
+yesBtn.addEventListener("click", () => {
+ question.innerHTML = "Aaaaa, ku juga suka kaaaa";
+ gif.src =
 "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
 
 noBtn.addEventListener("mouseover", () => {
 const noBtnRect = noBtn.getBoundingClientRect();
-const maxX = window.innerWidth noßtnRect.width;
-const maxY = window.innerHeight noBtnRect.height;
+const maxX = window.innerWidth - noßtnRect.width;
+const maxY = window.innerHeight - noBtnRect.height;
   
-const randomX = Math.floor(Math.random()* maxx);
+const randomX = Math.floor(Math.random() * maxx);
 const randomY = Math.floor(Math.random() * maxy);
 
 noBtn.style.left = randomX + "px";
